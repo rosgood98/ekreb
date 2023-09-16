62ca1ba7b7961e8b8f0db8845d6e71f055483bb8
rosgood98 <osgoodrichard@gmail.com>
2023-09-16 18:59:18 -0500

    more workflow updates

--- backend/test/Test.js
+++ backend/test/Test.js
@@ -8,11 +8,13 @@ const app = express();
 
 describe('GET Random Word', () => {
     it('should get a random word', () => {
+        this.timeout(5000);
         request(app)
         .get("https://random-word-api.herokuapp.com/word")
         .expect(200)
     });
     it('should fail to get a random word', () => {
+        this.timeout(5000);
         request(app)
         .get("https://random-word-api.herokuapp.com/getWord") // wrong endpoint
         .expect(400)
@@ -22,18 +24,21 @@ describe('GET Random Word', () => {
 describe('Scramble Word', () => {
     // check a scrambled word is not equal to the original
     it('should scramble a word', () => {
+        this.timeout(5000);
         const originalWord = 'hello';
         const scrambledWord = scramble(originalWord);
         expect(scrambledWord).to.not.equal(originalWord);
     });
     // check a scrambled word is equal to the original if the word is 1 char long
     it('should scramble a 1 char word to be the same word', () => {
+        this.timeout(5000);
         const originalWord = 'a';
         const scrambledWord = scramble(originalWord);
         expect(scrambledWord).to.equal(originalWord);
     });
     // check a 2 char scrambled word is separate from the original
     it('should scramble a 1 char word to be the same word', () => {
+        this.timeout(5000);
         const originalWord = 'hi';
         const scrambledWord = scramble(originalWord);
         expect(scrambledWord).to.not.equal(originalWord);
